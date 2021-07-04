f8d6c970b54e04792cacc1392d4d84c100e6ff87
Vamshi_Krishna <vamshikrishna.t21@gmail.com>
2021-07-04 19:52:10 -0400

    load image data

--- Traffic_Light_Classifier.py
+++ Traffic_Light_Classifier.py
@@ -9,3 +9,11 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg # for loading in images
 
 %matplotlib inline
+
+# Image data directories
+IMAGE_DIR_TRAINING = "traffic_light_images/training/"
+IMAGE_DIR_TEST = "traffic_light_images/test/"
+
+# Using the load_dataset function in helpers.py
+# Load training data
+IMAGE_LIST = helpers.load_dataset(IMAGE_DIR_TRAINING)
