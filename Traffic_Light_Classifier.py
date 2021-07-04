c514a56b19caa669d7602ff88f9a486ba5c5f2f8
Vamshi_Krishna <vamshikrishna.t21@gmail.com>
2021-07-04 19:40:24 -0400

    Import libraries and resources

---
+++ Traffic_Light_Classifier.py
@@ -0,0 +1,11 @@
+# import the libraries and resources
+
+import cv2 # computer vision library
+import helpers # helper functions
+
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg # for loading in images
+
+%matplotlib inline
