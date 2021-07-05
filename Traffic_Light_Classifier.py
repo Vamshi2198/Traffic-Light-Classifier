0f6496dff13648e0b4e22c7e8882877cdda5e1b3
Vamshi_Krishna <vamshikrishna.t21@gmail.com>
2021-07-05 04:26:47 -0400

    Implemented get_misclassified_images function

--- Traffic_Light_Classifier.py
+++ Traffic_Light_Classifier.py
@@ -185,5 +185,56 @@ def estimate_label(rgb_image):
     
     
     return predicted_label   
+
+# Using the load_dataset function in helpers.py
+# Load test data
+TEST_IMAGE_LIST = helpers.load_dataset(IMAGE_DIR_TEST)
+
+# Standardize the test data
+STANDARDIZED_TEST_LIST = standardize(TEST_IMAGE_LIST)
+
+# Shuffle the standardized test data
+random.shuffle(STANDARDIZED_TEST_LIST)
+
+# Constructs a list of misclassified images given a list of test images and their labels
+# This will throw an AssertionError if labels are not standardized (one-hot encoded)
+
+def get_misclassified_images(test_images):
+    # Track misclassified images by placing them into a list
+    misclassified_images_labels = []
+
+    # Iterate through all the test images
+    # Classify each image and compare to the true label
+    for image in test_images:
+
+        # Get true data
+        im = image[0]
+        true_label = image[1]
+        assert(len(true_label) == 3), "The true_label is not the expected length (3)."
+
+        # Get predicted label from your classifier
+        predicted_label = estimate_label(im)
+        assert(len(predicted_label) == 3), "The predicted_label is not the expected length (3)."
+
+        # Compare true and predicted labels 
+        if(predicted_label != true_label):
+            # If these labels are not equal, the image has been misclassified
+            misclassified_images_labels.append((im, predicted_label, true_label))
+            
+    # Return the list of misclassified [image, predicted_label, true_label] values
+    return misclassified_images_labels
+
+
+# Find all misclassified images in a given test set
+MISCLASSIFIED = get_misclassified_images(STANDARDIZED_TEST_LIST)
+
+# Accuracy calculations
+total = len(STANDARDIZED_TEST_LIST)
+num_correct = total - len(MISCLASSIFIED)
+accuracy = num_correct/total
+
+print('Accuracy: ' + str(accuracy))
+print("Number of misclassified images = " + str(len(MISCLASSIFIED)) +' out of '+ str(total))
+
     
 # %%
